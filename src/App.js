0abdf5185492b2781fa35454288fbc843b6ba385
plamen <plamen91ivanov@abv.bg>
2021-05-13 13:14:39 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,45 +1,34 @@
-import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import NewExpense from './components/NewExpense/NewExpense';
-import Expenses from './components/Expenses/Expenses';
+import SearchAlgorithm from './components/SearchAlgorithm/SearchAlgorithm';
+import Pathfinding from './components/Pathfinding/Pathfinding';
+import Snake from './components/SnakeBot/Snake';
 
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    title: 'Food',
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  { id: 'e2', title: 'Garden Tools', amount: 799.49, date: new Date(2021, 2, 12) },
-  {
-    id: 'e3',
-    title: 'Car',
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: 'e4',
-    title: 'New Desk',
-    amount: 150,
-    date: new Date(2021, 5, 12),
-  },
-];
+function App() {
+  // return (
+  //   <div className="App">
+  //     <SearchAlgorithm></SearchAlgorithm>
+  //     <Pathfinding></Pathfinding>
+  //     <Snake></Snake>
+  //     <div className="card-container"></div>
+  //   </div>
+  // );
+  const [state, setState] = useState('start')
 
-const App = () => {
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  triggerAddTripState = () => {
+    setState('add-trip')
+  }
 
-  const addExpenseHandler = (expense) => {
-    setExpenses((prevExpenses) => {
-      return [expense, ...prevExpenses];
-    });
-  }; 
-  
   return (
     <div>
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
+      {state === 'start' && (
+        <AddTripButton addTrip={this.triggerAddTripState} />
+      )}
+
+      {state === 'add-trip' && <AnotherComponent />}
     </div>
-  );
-};
+  )
+}
 
 export default App;
